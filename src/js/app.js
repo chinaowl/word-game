e92cd610e9c6be04c77d0b26f1b30b588cea86ba
China Wang <chwang@thoughtworks.com>
2015-10-09 21:49:01 -0700

    less brute force, more random but it works!

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,11 @@
     var dictionary = require('./dictionary.json');
     var _uniq = require('lodash/array/uniq');
     var _intersection = require('lodash/array/intersection');
-
+    var _difference = require('lodash/array/difference');
+    var _union = require('lodash/array/union');
+    var _remove = require('lodash/array/remove');
+    var _forEach = require('lodash/collection/forEach');
+    var _includes = require('lodash/collection/includes');
 
     var vm = {
         word: ko.observable(),
@@ -39,33 +43,73 @@
     }
 
     function guessWord(word) {
-        var guess, guessCount = 0, success = false, allGuesses = [], tempIndex = 0;
+        var success = false;
+        var allGuesses = [];
+        var found = [];
+        var eliminated = [];
+        var unknown = 'abcdefghijklmnopqrstuvwxyz'.split('');
+        var possibleWords = allWords.slice();
+        var countToGuessMap = [];
 
-        while (guessCount < allWords.length) {
-        	guess = allWords[tempIndex++];
-        	allGuesses.push({
-        		guess: guess,
-        		count: getCorrectLetterCount(word, guess)
-        	});
+        for (var i = 0; i <= 5; i++) {
+            countToGuessMap[i] = [];
+        }
 
-        	if (guess === word) {
-        		success = true;
-        		break;
-        	}
+        function getRandomIndex(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
 
-        	guessCount++;
+        function eliminateLetter(letter) {
+            if (!_includes(eliminated, letter)) {
+                eliminated.push(letter);
+                unknown = _remove(unknown, function(x) {
+                    return x === letter;
+                });
+                possibleWords = _difference(possibleWords, dictionary[letter]);
+            }
+        }
+
+        while (possibleWords.length > 0) {
+            // make a guess
+            var index = getRandomIndex(0, possibleWords.length - 1);
+            var guess = possibleWords[index];
+            var count = getCorrectLetterCount(word, guess);
+
+            // log guess
+            countToGuessMap[count].push(guess);
+            allGuesses.push({
+                guess: guess,
+                count: count
+            });
+
+            // check guess
+            if (guess === word) {
+                success = true;
+                break;
+            }
+
+            possibleWords.splice(index, 1);
+
+            // analyze guess
+            if (count === 0) {
+                _forEach(guess.split(''), function(letter) {
+                    eliminateLetter(letter);
+                });
+            } else {
+                // TODO: more stuff here...
+            }
         }
 
         if (success) {
-        	vm.allGuesses = allGuesses;
-        	vm.showResults(true);
+            vm.allGuesses = allGuesses;
+            vm.showResults(true);
         } else {
-        	vm.couldNotGuess(true);
+            vm.couldNotGuess(true);
         }
     }
 
     function getCorrectLetterCount(actual, guess) {
-    	return _intersection(actual.split(''), guess.split('')).length;
+        return _intersection(actual.split(''), guess.split('')).length;
     }
 
     ko.applyBindings(vm);
